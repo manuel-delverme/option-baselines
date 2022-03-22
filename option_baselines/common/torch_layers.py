7b7a563dd45713a4102e0e722131abebda1b0afa
manuel <manuel.delverme@gmail.com>
2022-03-22 19:23:40 -0400

    1 extra layer for smoother gradients.

--- option_baselines/common/torch_layers.py
+++ option_baselines/common/torch_layers.py
@@ -24,8 +24,8 @@ class SimpleCNN(torch_layers.BaseFeaturesExtractor):
             nn.ReLU(),
             # nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
             # nn.ReLU(),
-            # nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
-            # nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
             nn.Flatten(),
         )
 
