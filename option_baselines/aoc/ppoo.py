45290a4f323aeec1ec40f9fc2258fbf79aac9cbc
manuel <delverme@equilibretechnologies.com>
2023-03-09 17:09:54 +0100

    https://wandb.ai/delvermm/option-base/runs/py7ly88n
    7r 3c, stable

--- option_baselines/aoc/ppoo.py
+++ option_baselines/aoc/ppoo.py
@@ -362,8 +362,10 @@ class PPOO(OnPolicyAlgorithm):
         self.logger.record("train/clip_fraction", np.mean(clip_fractions))
 
         # self.logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
-        self.logger.record("entropy/option", -entropies.mean().item())
-        self.logger.record("entropy/meta", -meta_entropies.mean().item())
+        self.logger.record("entropy/option", entropies.mean().item())
+        self.logger.record("entropy/action_prob", torch.exp(action_log_prob).mean().item())
+        self.logger.record("entropy/meta", meta_entropies.mean().item())
+        self.logger.record("entropy/meta_action_prob", torch.exp(meta_log_prob).mean().item())
 
         self.logger.record("train/grad_norm", grad_norm)
         for k, v in grad_means.items():
